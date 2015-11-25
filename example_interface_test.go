d83aa2cc5fc7687941f8613461a2fd2cce442635
Tomoya.Miwa <tomoyann@gmail.com>
2015-11-26 00:36:31 +0900

    Add Example

---
+++ example_interface_test.go
@@ -0,0 +1,26 @@
+package im920_test
+
+import (
+	"fmt"
+	"github.com/tomoya0x00/go-im920"
+	"time"
+)
+
+func Example() {
+	c := &im920.Config{Name: "COM4", ReadTimeout: time.Second * 1}
+	im, err := im920.Open(c)
+	if err != nil {
+		fmt.Printf("Failed to open: %s", err)
+		return
+	}
+	defer im.Close()
+
+	data := []byte("0123456789")
+	_, err = im.Write(data)
+	if err != nil {
+		fmt.Printf("Failed to write: %s", err)
+		return
+	}
+
+	// Output:
+}
