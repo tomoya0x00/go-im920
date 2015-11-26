9835e76839faf52ddf04ab8f957aae246824e2a2
Tomoya.Miwa <tomoyann@gmail.com>
2015-11-27 01:15:03 +0900

    Add ExampleRead

---
+++ example_test.go
@@ -0,0 +1,30 @@
+package im920_test
+
+import (
+	"fmt"
+	"github.com/tomoya0x00/go-im920"
+	"time"
+)
+
+func ExampleRead() {
+    // This test will not be run, it has no "Output:" comment.
+	c := &im920.Config{Name: "COM4", ReadTimeout: time.Second * 1}
+	im, err := im920.Open(c)
+	if err != nil {
+		fmt.Printf("Failed to open: %s\n", err)
+		return
+	}
+	defer im.Close()
+
+    for i := 0; i < 20; i++ {
+        buf := make([]byte, 64)
+        n, err := im.Read(buf)
+        if err != nil {
+            fmt.Printf("Failed to read: %s\n", err)
+            continue
+        }
+
+        fmt.Printf("Read Data: %v\n", buf[:n])
+    }
+}
+
