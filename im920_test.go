657821a9da9295d8551e292403d66da24c52a4fd
Tomoya.Miwa <tomoyann@gmail.com>
2015-11-26 22:57:35 +0900

    Add tests to TestWrite()

--- im920_test.go
+++ im920_test.go
@@ -37,15 +37,33 @@ func (serial *fakeSerial) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+var WriteTests = []struct {
+	in_writeData   []byte
+	in_dummyData   []byte
+	out_errorIsNil bool
+}{
+	{[]byte("01234567890"), []byte("OK\r\n"), true},
+	{[]byte("01234567890"), []byte("NG\r\n"), false},
+	{[]byte("01234567890"), []byte("HOGE\r\n"), false},
+	{[]byte("01234567890"), []byte(""), false},
+	{[]byte(""), []byte("OK\r\n"), true},
+	{[]byte(""), []byte("NG\r\n"), false},
+	{[]byte(""), []byte("HOGE\r\n"), false},
+	{[]byte(""), []byte(""), false},
+}
+
 func TestWrite(t *testing.T) {
 	serial := newFakeSerial()
 	im := &IM920{serial}
 
-	serial.dummyData = []byte("OK\r\n")
-
-	data := []byte("0123456789012345678901234567890123456789012345678901234567890123456789")
-	_, err := im.Write(data)
-	if err != nil {
-		t.Errorf("Failed to write: %s", err)
+	for i, tt := range WriteTests {
+		serial.dummyData = tt.in_dummyData
+		data := tt.in_writeData
+		_, err := im.Write(data)
+		if (tt.out_errorIsNil && (err != nil)) ||
+			(!tt.out_errorIsNil && (err == nil)) {
+			t.Errorf("[%d]Write(%v) => %v, want errorIsNil = %v",
+				i, tt.in_writeData, err, tt.out_errorIsNil)
+		}
 	}
 }
