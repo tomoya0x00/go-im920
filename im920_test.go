d92c459d2540c076aa620223f2c8e6f6d95f0ac2
Tomoya.Miwa <tomoyann@gmail.com>
2015-12-02 21:10:56 +0900

    Add tests to receive() and IssueCommand()

--- im920_test.go
+++ im920_test.go
@@ -39,6 +39,98 @@ func (serial *fakeSerial) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+var ReceiveTests = []struct {
+	in_out         []byte
+	out_errorIsNil bool
+}{
+	{
+		[]byte("00,06E5,B5:0A,1F,76,00,00,00,00,00\r\n"), true,
+	},
+	{
+		[]byte("00,06E5,B5:0A,1F,76\r\n"), true,
+	},
+	{
+		[]byte("00,06E5,B5:0A\r\n"), true,
+	},
+	{
+		[]byte("00,06E5,B5:0A,1F,76"), true,
+	},
+	{
+		[]byte("00,06E5,B5:\r\n"), true,
+	},
+	{
+		[]byte("00,06E5,B5\r\n"), true,
+	},
+	{
+		[]byte(""), false,
+	},
+}
+
+func TestReceive(t *testing.T) {
+	serial := newFakeSerial()
+	im := &IM920{s: serial, readTimeout: 100 * time.Millisecond}
+
+	buf := make([]byte, maxReadSize)
+
+	for i, tt := range ReceiveTests {
+		serial.dummyData = tt.in_out
+		n, err := im.receive(buf)
+		if (tt.out_errorIsNil && (err != nil)) ||
+			(!tt.out_errorIsNil && (err == nil)) {
+			t.Errorf("[%d]receive() => %v, want errorIsNil = %v",
+				i, err, tt.out_errorIsNil)
+		}
+		if !bytes.Equal(buf[:n], tt.in_out) {
+			t.Errorf("[%d]receive() => %v, want data = %v",
+				i, buf, tt.in_out)
+		}
+	}
+}
+
+var IssueCommandTests = []struct {
+	in_cmd         string
+	in_param       string
+	in_dummyData   []byte
+	out            []byte
+	out_errorIsNil bool
+}{
+	{
+		"HOGE", "HUGA", []byte("OK\r\n"),
+		[]byte("OK\r\n"), true,
+	},
+	{
+		"HOGE", "HUGA", []byte("NG\r\n"),
+		[]byte("NG\r\n"), false,
+	},
+	{
+		"HOGE", "HUGA", []byte("FUGA\r\n"),
+		[]byte("FUGA\r\n"), true,
+	},
+	{
+		"HOGE", "HUGA", []byte(""),
+		[]byte(""), false,
+	},
+}
+
+func TestIssueCommand(t *testing.T) {
+	serial := newFakeSerial()
+	im := &IM920{s: serial, readTimeout: 100 * time.Millisecond}
+
+	for i, tt := range IssueCommandTests {
+		serial.dummyData = tt.in_dummyData
+		resp, err := im.IssueCommand(tt.in_cmd, tt.in_param)
+		if (tt.out_errorIsNil && (err != nil)) ||
+			(!tt.out_errorIsNil && (err == nil)) {
+			t.Errorf("[%d]IssueCommand(%v, %v) => %v, want errorIsNil = %v",
+				i, tt.in_cmd, tt.in_param, err, tt.out_errorIsNil)
+		}
+		if !bytes.Equal(resp, tt.out) {
+			t.Errorf("[%d]IssueCommand() => %v, want data = %v",
+				i, resp, tt.out)
+		}
+	}
+}
+
 var WriteTests = []struct {
 	in_writeData   []byte
 	in_dummyData   []byte
@@ -56,7 +148,7 @@ var WriteTests = []struct {
 
 func TestWrite(t *testing.T) {
 	serial := newFakeSerial()
-	im := &IM920{s: serial, readTimeout: 1 * time.Second}
+	im := &IM920{s: serial, readTimeout: 100 * time.Millisecond}
 
 	for i, tt := range WriteTests {
 		serial.dummyData = tt.in_dummyData
@@ -72,43 +164,42 @@ func TestWrite(t *testing.T) {
 
 var ReadTests = []struct {
 	in             []byte
-	out_data       []byte
-	out_n          int
+	out            []byte
 	out_errorIsNil bool
 }{
 	{
 		[]byte("00,06E5,B5:0A,1F,76,00,00,00,00,00\r\n"),
-		[]byte{0x0A, 0x1F, 0x76, 0x00, 0x00, 0x00, 0x00, 0x00}, 8, true,
+		[]byte{0x0A, 0x1F, 0x76, 0x00, 0x00, 0x00, 0x00, 0x00}, true,
 	},
 	{
 		[]byte("00,06E5,B5:0A,1F,76\r\n"),
-		[]byte{0x0A, 0x1F, 0x76}, 3, true,
+		[]byte{0x0A, 0x1F, 0x76}, true,
 	},
 	{
 		[]byte("00,06E5,B5:0A\r\n"),
-		[]byte{0x0A}, 1, true,
+		[]byte{0x0A}, true,
 	},
 	{
 		[]byte("00,06E5,B5:0A,1F,76"),
-		[]byte{}, 0, false,
+		[]byte{}, false,
 	},
 	{
 		[]byte("00,06E5,B5:\r\n"),
-		[]byte{}, 0, false,
+		[]byte{}, false,
 	},
 	{
 		[]byte("00,06E5,B5\r\n"),
-		[]byte{}, 0, false,
+		[]byte{}, false,
 	},
 	{
 		[]byte(""),
-		[]byte{}, 0, false,
+		[]byte{}, false,
 	},
 }
 
 func TestRead(t *testing.T) {
 	serial := newFakeSerial()
-	im := &IM920{s: serial, readTimeout: 1 * time.Second}
+	im := &IM920{s: serial, readTimeout: 100 * time.Millisecond}
 
 	buf := make([]byte, maxReadSize)
 
@@ -120,14 +211,9 @@ func TestRead(t *testing.T) {
 			t.Errorf("[%d]Read() => %v, want errorIsNil = %v",
 				i, err, tt.out_errorIsNil)
 		}
-		if !bytes.Equal(buf[:n], tt.out_data) {
+		if !bytes.Equal(buf[:n], tt.out) {
 			t.Errorf("[%d]Read() => %v, want data = %v",
-				i, buf, tt.out_data)
-		}
-
-		if n != tt.out_n {
-			t.Errorf("[%d]Read() => %v, want n = %v",
-				i, n, tt.out_n)
+				i, buf, tt.out)
 		}
 	}
 }
