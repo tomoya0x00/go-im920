893b167f3aaf02eadc230247f2008f9bbfe5023b
Tomoya.Miwa <tomoyann@gmail.com>
2015-12-04 01:24:03 +0900

    Refactor error strings

--- im920.go
+++ im920.go
@@ -41,7 +41,7 @@ func Open(c *Config) (*IM920, error) {
 	sc := &serial.Config{Name: c.Name, Baud: defaultBps, ReadTimeout: t}
 	s, err := serial.OpenPort(sc)
 	if err != nil {
-		return &IM920{}, fmt.Errorf("Filed to open: %s", err)
+		return &IM920{}, fmt.Errorf("error: OpenPort failed: %s", err)
 	}
 
 	return &IM920{s: s, readTimeout: c.ReadTimeout}, nil
@@ -55,13 +55,13 @@ func strToUint16(s string) (val uint16, err error) {
 
 	switch len(b) {
 	case 0:
-		err = fmt.Errorf("Failed to decode : no data (%v)", []byte(s))
+		err = fmt.Errorf("error: Decode failed: data not found (%s)", s)
 	case 1:
 		val = uint16(b[0])
 	case 2:
 		val = binary.BigEndian.Uint16(b)
 	default:
-		err = fmt.Errorf("Failed to decode : invalid size (%v)", b)
+		err = fmt.Errorf("error: Decode failed: invalid size (%v)", len(b))
 	}
 
 	return
@@ -70,26 +70,26 @@ func strToUint16(s string) (val uint16, err error) {
 func parseReadHeaders(s string) (info ReadInfo, err error) {
 	headers := strings.Split(s, ",")
 	if len(headers) < 3 {
-		err = fmt.Errorf("Failed to split headers : %s", s)
+		err = fmt.Errorf("error: Split headers failed: %s", s)
 		return
 	}
 
 	node, derr := hex.DecodeString(headers[0])
 	if derr != nil {
-		err = fmt.Errorf("Failed to decode node(%s) : %s", headers[0], derr)
+		err = fmt.Errorf("error: Decode Node failed (%s): %s", headers[0], derr)
 		return
 	}
 	info.Node = uint8(node[0])
 
 	info.FromId, derr = strToUint16(headers[1])
 	if err != nil {
-		err = fmt.Errorf("Failed to decode fromId(%s) : %s", headers[1], derr)
+		err = fmt.Errorf("error: Decode FromId failed (%s): %s", headers[1], derr)
 		return
 	}
 
 	rssi, derr := hex.DecodeString(headers[2])
 	if err != nil {
-		err = fmt.Errorf("Failed to decode rssi(%s) : %s", headers[2], derr)
+		err = fmt.Errorf("error: Decode Rssi failed (%s): %s", headers[2], derr)
 		return
 	}
 	info.Rssi = uint8(rssi[0])
@@ -107,13 +107,13 @@ func (im *IM920) receive(p []byte) (readed int, err error) {
 		select {
 		case <-timer.C:
 			if readed == 0 {
-				err = fmt.Errorf("Failed to read: no data")
+				err = fmt.Errorf("error: Read failed: no data")
 			}
 			return
 		default:
 			n, rerr := im.s.Read(p[readed : readed+1])
 			if rerr != nil {
-				err = fmt.Errorf("Failed to read : %s", rerr)
+				err = fmt.Errorf("error: Read failed: %s", rerr)
 				return
 			}
 			if n > 0 && !readedInitialbyte {
@@ -137,7 +137,7 @@ func (im *IM920) IssueCommand(cmd, param string) (resp []byte, err error) {
 	s := []string{cmd, param, "\r\n"}
 	_, werr := im.s.Write([]byte(strings.Join(s, " ")))
 	if werr != nil {
-		err = fmt.Errorf("Failed to write: %s", werr)
+		err = fmt.Errorf("error: Write failed: %s", werr)
 		return
 	}
 
@@ -145,11 +145,11 @@ func (im *IM920) IssueCommand(cmd, param string) (resp []byte, err error) {
 	rcv := make([]byte, maxReadSize)
 	rcved, rerr := im.receive(rcv)
 	if rerr != nil {
-		err = fmt.Errorf("Failed to receive: %s", rerr)
+		err = fmt.Errorf("error: receive failed: %s", rerr)
 		return
 	}
 	if rcved == 0 {
-		err = fmt.Errorf("Failed to receive: no data")
+		err = fmt.Errorf("error: receive failed: no data")
 		return
 	}
 
@@ -167,7 +167,7 @@ func (im *IM920) IssueCommandNormal(cmd, param string) error {
 	}
 
 	if !bytes.Equal(resp, []byte("OK\r\n")) {
-		return fmt.Errorf("Unknown response: %v", resp)
+		return fmt.Errorf("error: Unknown response (%v)", resp)
 	}
 
 	return nil
@@ -182,7 +182,7 @@ func (im *IM920) IssueCommandRespStr(cmd, param string) (resp string, err error)
 
 	dataEnd := strings.LastIndex(string(rcv), "\r\n")
 	if strings.Index(string(rcv), "\r\n") < 0 {
-		err = fmt.Errorf("Failed to find the end of data : %v", rcv)
+		err = fmt.Errorf("error: not found the end of data (%v)", rcv)
 		return
 	}
 
@@ -220,7 +220,7 @@ func (im *IM920) IssueCommandRespNums(cmd, param string) (resp []uint16, err err
 	}
 
 	if len(resp) == 0 {
-		err = fmt.Errorf("Failed to receive: no data")
+		err = fmt.Errorf("error: receive failed: no data")
 	}
 
 	return
@@ -249,34 +249,34 @@ func (im *IM920) Read(p []byte) (n int, err error) {
 
 	readed, err := im.receive(buf)
 	if err != nil {
-		return 0, fmt.Errorf("Failed to read: %s", err)
+		return 0, fmt.Errorf("error: Read failed: %s", err)
 	}
 	if readed == 0 {
-		return 0, fmt.Errorf("Failed to read: no data")
+		return 0, fmt.Errorf("error: Read failed: no data")
 	}
 
 	strs := strings.Split(string(buf), ":")
 	if len(strs) < 2 {
-		return 0, fmt.Errorf("Failed to split header and data : %s", string(buf))
+		return 0, fmt.Errorf("error: Split header and data failed (%s)", string(buf))
 	}
 
 	info, err := parseReadHeaders(strs[0])
 	if err != nil {
-		return 0, fmt.Errorf("Failed to parseReadHeaders: %s", err)
+		return 0, fmt.Errorf("error: parseReadHeaders failed: %s", err)
 	}
 
 	dataEnd := strings.Index(strs[1], "\r\n")
 	if dataEnd < 0 {
-		return 0, fmt.Errorf("Failed to find the end of data : %s", strs[1])
+		return 0, fmt.Errorf("error: not found the end of data (%s)", strs[1])
 	}
 
 	dataStr := strings.Replace(strs[1][:dataEnd], ",", "", -1)
 	data, err := hex.DecodeString(dataStr)
 	if err != nil {
-		return 0, fmt.Errorf("Failed to decode(%s) : %s", dataStr, err)
+		return 0, fmt.Errorf("error: Decode failed (%s): %s", dataStr, err)
 	}
 	if len(data) == 0 {
-		return 0, fmt.Errorf("Failed to decode : no data (%s)", strs[1])
+		return 0, fmt.Errorf("error: Decode failed: no data")
 	}
 
 	n = copy(p, data)
